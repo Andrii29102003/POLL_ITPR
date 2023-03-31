d4d1760b0ac2cba5015ca7017954bb1add733d8c
Andrii29102003 <dandrii2910@gmail.com>
2023-03-31 09:21:46 +0300

    перероблено index.html

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
-from flask import request
 from flask import Flask
+from flask import request
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
