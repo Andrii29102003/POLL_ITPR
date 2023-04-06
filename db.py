cc600e5bfaad5a0571184d2bfb21aa25af742cad
Andrii29102003 <dandrii2910@gmail.com>
2023-04-06 11:08:35 +0300

    доробив db.py

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 import json
 
 #id = int
+#passw = str
 #links = json data
 #marks = json data
 #tmp = json data
@@ -11,30 +12,37 @@ class DB():
     def __init__(self, file_name):     #підключення до бази даних створення таблиці в ній таблиці
         self.con = sqlite3.connect(file_name)
         self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS ask(id INTEGER PRIMARY KEY, pass CHAR(9), links CHAR(12000), marks(100), tmp TEXT)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS ask(id INTEGER PRIMARY KEY, passw CHAR(9), links CHAR(12000), marks(100), tmp TEXT)")
         self.con.commit()
     
     def new(self):      #створення зпису нового пустого опитування
-        self.cur.execute('INSERT INTO ask(pass, links, marks, tmp) VALUES("", "", "", "")')
+        self.cur.execute('INSERT INTO ask(passw, links, marks, tmp) VALUES("", "", "", "")')
         self.con.commit()
-        self.cur.execute('SELECT id FROM ask WHERE pass="" AND links="" AND marks="" AND tmp=""')
+        self.cur.execute('SELECT id FROM ask WHERE passw="" AND links="" AND marks="" AND tmp=""')
+        self.con.commit()
         return self.cur.fetchone()[0]
 
     def add(self, passw, links, marks, tmp):     #додавання нового опитування з даними
-        links = json.dumps(links)
-        marks = json.dumps(marks)
-        tmp = json.dumps(tmp)
-        self.cur.execute("INSERT INTO ask(pass, links, marks, tmp) VALUES(?, ?, ?, ?)", (passw, links, marks, tmp))
+        self.cur.execute("INSERT INTO ask(passw, links, marks, tmp) VALUES(?, ?, ?, ?)", (json.dumps(passw), json.dumps(links), json.dumps(marks), json.dumps(tmp)))
         self.con.commit()
-        self.cur.execute("SELECT id FROM ask WHERE pass=? AND links=? AND marks=? AND tmp=?", (passw, links, marks, tmp))
+        self.cur.execute("SELECT id FROM ask WHERE passw=? AND links=? AND marks=? AND tmp=?", (json.dumps(passw), json.dumps(links), json.dumps(marks), json.dumps(tmp)))
+        self.con.commit()
         return self.cur.fetchone()[0]
     
     def get(self, id):      #отримання опитування по id
         self.cur.execute("SELECT * FROM ask WHERE id=?", (id))
-        return self.cur.fetchone()
+        self.con.commit()
+        data={}
+        data["id"] = self.cur.fetchone()[0]
+        data["passw"] = self.cur.fetchone()[1]
+        data["links"] = json.loads(self.cur.fetchone()[2])
+        data["marks"] = json.loads(self.cur.fetchone()[3])
+        data["tmp"] = json.loads(self.cur.fetchone()[4])
+        return data    #повертає дані в словнику data з ключами id, passw, links, marks, tmp
     
     def get_all(self):      #отримання даних всіх опитувань
         self.cur.execute("SELECT * FROM ask")
+        self.con.commit()
         return self.cur.fetchall()
     
     def delete(self, id):       #видалення опитування по id
@@ -42,11 +50,8 @@ class DB():
         self.con.commit()
         return True
     
-    def update(self, id, passw, links, marks, tmp):      #оновлення даних опитування по id
-        links = json.dumps(links)
-        marks = json.dumps(marks)
-        tmp = json.dumps(tmp)
-        self.cur.execute("UPDATE ask SET pass=?, links=?, marks=?, tmp=? WHERE id=?", (passw, links, marks, tmp, id))
+    def update(self, id, links, marks, tmp):      #оновлення даних опитування по id
+        self.cur.execute("UPDATE ask SET links=?, marks=?, tmp=? WHERE id=?", (json.dumps(links), json.dumps(marks), json.dumps(tmp), id))
         self.con.commit()
         return True
     
