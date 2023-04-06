09174164a5c2e8c70a1408f4d10789c50e5a1394
Andrii29102003 <dandrii2910@gmail.com>
2023-04-06 10:08:18 +0300

    READMY

--- db.py
+++ db.py
@@ -5,23 +5,23 @@ class db():
     def __init__(self):     #підключення до бази даних створення таблиці в ній таблиці
         self.con = sqlite3.connect("data.db")
         self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS ask(id INTEGER PRIMARY KEY, pass CHAR(9), links CHAR(12000), marks(100) else TEXT)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS ask(id INTEGER PRIMARY KEY, pass CHAR(9), links CHAR(12000), marks(100), tmp TEXT)")
         self.con.commit()
     
     def new(self):      #створення зпису нового пустого опитування
-        self.cur.execute('INSERT INTO ask(pass, links, marks) VALUES("", "", "")')
+        self.cur.execute('INSERT INTO ask(pass, links, marks, tmp) VALUES("", "", "", "")')
         self.con.commit()
-        self.cur.execute('SELECT id FROM ask WHERE pass="" AND links="" AND marks=""')
+        self.cur.execute('SELECT id FROM ask WHERE pass="" AND links="" AND marks="" AND tmp=""')
         return self.cur.fetchone()[0]
 
-    def add(self, passw, links, marks):     #додавання нового опитування з даними
-        self.cur.execute("INSERT INTO ask(pass, links, marks) VALUES(?, ?, ?)", (passw, links, marks))
+    def add(self, passw, links, marks, tmp):     #додавання нового опитування з даними
+        self.cur.execute("INSERT INTO ask(pass, links, marks, tmp) VALUES(?, ?, ?, ?)", (passw, links, marks, tmp))
         self.con.commit()
-        self.cur.execute("SELECT id FROM ask WHERE pass=? AND links=? AND marks=?", (passw, links, marks))
+        self.cur.execute("SELECT id FROM ask WHERE pass=? AND links=? AND marks=? AND tmp=?", (passw, links, marks, tmp))
         return self.cur.fetchone()[0]
     
     def get(self, id):      #отримання опитування по id
-        self.cur.execute("SELECT * FROM ask WHERE id=?", (id,))
+        self.cur.execute("SELECT * FROM ask WHERE id=?", (id))
         return self.cur.fetchone()
     
     def get_all(self):      #отримання даних всіх опитувань
@@ -29,12 +29,12 @@ class db():
         return self.cur.fetchall()
     
     def delete(self, id):       #видалення опитування по id
-        self.cur.execute("DELETE FROM ask WHERE id=?", (id,))
+        self.cur.execute("DELETE FROM ask WHERE id=?", (id))
         self.con.commit()
         return True
     
-    def update(self, id, passw, links, marks):      #оновлення даних опитування по id
-        self.cur.execute("UPDATE ask SET pass=?, links=?, marks=? WHERE id=?", (passw, links, marks, id))
+    def update(self, id, passw, links, marks, tmp):      #оновлення даних опитування по id
+        self.cur.execute("UPDATE ask SET pass=?, links=?, marks=?, tmp=? WHERE id=?", (passw, links, marks, tmp, id))
         self.con.commit()
         return True
     
