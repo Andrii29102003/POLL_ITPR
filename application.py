7d56d5cbeff5d01953a7ff944cb8a11f83a1ecff
Andrii29102003 <dandrii2910@gmail.com>
2023-05-07 10:23:32 +0300

    some restyle ask

--- application.py
+++ application.py
@@ -51,8 +51,8 @@ def index():
 def ask():
     """TESTING"""    
     db = get_db()
-    result = db.execute_query("SELECT * FROM ask")
-    print('result: ', result)
+    #result = db.execute_query("SELECT * FROM ask")
+    #print('result: ', result)
     #urls = ['https://assets.aboutamazon.com/dims4/default/42868bd/2147483647/strip/true/crop/1279x720+0+0/resize/1320x743!/format/webp/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2F06%2Ff1%2F8767bdab489e8e2780a3f870e8c5%2Falexa-for-pets-1.jpg', "https://assets.aboutamazon.com/dims4/default/42868bd/2147483647/strip/true/crop/1279x720+0+0/resize/1320x743!/format/webp/quality/90/?url=https%3A%2F%2Famazon-blogs-brightspot.s3.amazonaws.com%2F06%2Ff1%2F8767bdab489e8e2780a3f870e8c5%2Falexa-for-pets-1.jpg", "https://npr.brightspotcdn.com/dims4/default/4c6a59a/2147483647/strip/true/crop/4032x2268+0+378/resize/1200x675!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fwxxi%2Ffiles%2F202004%2Fsassy_sick.jpg"]
     return render_template('ask.html')
 
